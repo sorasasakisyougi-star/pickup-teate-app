bcdc605f7dcb9720e4877ae84370c23ca533f0f5
sorasasakisyougi-star <sorasasakisyougi@gmail.com>
2026-03-09 20:23:34 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import "./globals.css";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 export const metadata: Metadata = {
   title: "pickup-teate-app",
   description: "pickup-teate-app",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
+  themeColor: "#020817",
 };
 
 export default function RootLayout({
